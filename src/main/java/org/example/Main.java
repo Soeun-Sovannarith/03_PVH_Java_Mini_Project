fd81293e9f2e7b8ca85491a8bb8c0cf60cdba5be
RithCB <seounsovanarith@gmail.com>
2026-03-02 14:30:52 +0700

    add new structure

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,10 +5,6 @@ import org.example.Utilities.DatabaseUtil;
 
 public class Main {
     public static void main(String[] args) {
-//      DatabaseUtil.getConnection();
-        ReadProduct readProduct=new ReadProduct();
-        readProduct.readProduct();
-        Update update= new Update();
-        update.update();
+        StockManagement.run();
     }
 }
--- src/main/java/org/example/StockManagement.java
+++ src/main/java/org/example/StockManagement.java
@@ -1,4 +1,13 @@
 package org.example;
 
+
+import org.example.Controller.StockController;
+import org.example.Utilities.DatabaseUtil;
+
 public class StockManagement {
+    public static void run(){
+        DatabaseUtil.getConnection();
+        StockController.readProduct();
+        StockController.update();
+    }
 }
