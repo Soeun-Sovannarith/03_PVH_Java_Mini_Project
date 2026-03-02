c772822ca0c750f5d6c66ec7ebae6b13c210ede9
Veasna <somveasna00@gmail.com>
2026-03-02 15:35:09 +0700

    add validation  and color

--- src/main/java/org/example/Services/ProductServiceImpl.java
+++ src/main/java/org/example/Services/ProductServiceImpl.java
@@ -1,7 +1,18 @@
 package org.example.Services;
 
+import org.example.Models.Product;
+
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
-public class ProductServiceImpl {
+public class ProductServiceImpl implements ProductService {
+    List<Product> productWrite=new ArrayList<>();
+    @Override
+    public Product writeProduct() {
+        Scanner sc=new Scanner(System.in);
 
+
+        return null;
+    }
 }
